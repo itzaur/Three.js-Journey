efbc4f82c3e163532101ee54c0d3435abf14c0f3
itzaur <preneur85@gmail.com>
2025-09-18 14:27:38 +0300

    feat: add the design of the card

---
+++ src/components/Card.tsx
@@ -0,0 +1,38 @@
+import { Link } from '@tanstack/react-router';
+
+function Card({
+  sketch,
+  variant,
+}: {
+  sketch: string;
+  variant: { type: 'r3f' | 'vanilla'; id: string };
+}) {
+  return (
+    <Link
+      key={variant.id}
+      className='lesson-card card'
+      to='/lessons/$sketch/$type/$id'
+      params={{
+        sketch,
+        type: variant.type,
+        id: variant.id,
+      }}
+    >
+      <div className='card__cover'>
+        <img
+          className='cover-preview'
+          src='https://images.pexels.com/photos/861440/pexels-photo-861440.jpeg'
+          alt=''
+        />
+      </div>
+      <div className='card__info'>
+        <span className='card__info-dot'></span>
+        <span className='card__info-text'>{variant.type}</span>
+        <span className='card__info-dot'></span>
+      </div>
+      <div className='card__bg' />
+    </Link>
+  );
+}
+
+export default Card;
--- src/routes/lessons/route.tsx
+++ src/routes/lessons/route.tsx
@@ -1,5 +1,7 @@
 import { getLessons } from '@/server/getLessons';
 import { createFileRoute, Link, Outlet } from '@tanstack/react-router';
+import Card from '@/components/Card';
+import { useEffect } from 'react';
 
 export const Route = createFileRoute('/lessons')({
   loader: () => getLessons(),
@@ -11,28 +13,70 @@ function Lessons() {
 
   console.log('lessons', lessons);
 
+  useEffect(() => {
+    const cards = document.querySelectorAll<HTMLDivElement>('.card');
+
+    const handlePointerMove = (e: PointerEvent) => {
+      cards.forEach((card) => {
+        const rect = card.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        card.style.setProperty('--mouse-x', `${x}px`);
+        card.style.setProperty('--mouse-y', `${y}px`);
+      });
+    };
+
+    document
+      .querySelector<HTMLDivElement>('[data-grid]')
+      ?.addEventListener('pointermove', handlePointerMove);
+
+    return () => {
+      document
+        .querySelector<HTMLDivElement>('[data-grid]')
+        ?.removeEventListener('pointermove', handlePointerMove);
+    };
+  }, []);
+
   return (
     <div className='home'>
-      <aside className='lessons'>
-        <h2>Lessons</h2>
+      <aside className='lessons' data-grid>
+        <h2 className='lessons__title'>Lessons</h2>
 
         {lessons.map((lesson) => (
+          // <Card
+          //   key={lesson.title}
+          //   title={lesson.title}
+          //   variants={lesson.variants}
+          //   sketch={lesson.sketch}
+          // />
+
           <div key={lesson.title} className='lesson-block'>
             <h3 className='lesson-block__title'>{lesson.title}</h3>
             <div className='lesson-block__grid'>
               {lesson.variants.map((variant) => (
-                <Link
+                <Card
                   key={variant.id}
-                  className='lesson-card'
-                  to='/lessons/$sketch/$type/$id'
-                  params={{
-                    sketch: lesson.sketch,
-                    type: variant.type,
-                    id: variant.id,
-                  }}
-                >
-                  {variant.type.toUpperCase()}
-                </Link>
+                  variant={variant}
+                  sketch={lesson.sketch}
+                />
+                // <Link
+                //   key={variant.id}
+                //   className='lesson-card'
+                //   to='/lessons/$sketch/$type/$id'
+                //   params={{
+                //     sketch: lesson.sketch,
+                //     type: variant.type,
+                //     id: variant.id,
+                //   }}
+                // >
+                //   <img
+                //     className='cover-preview'
+                //     src='https://placehold.co/400x400'
+                //     alt=''
+                //   />
+                //   {variant.type}
+                // </Link>
               ))}
             </div>
           </div>
